cd7fda6189884230bb0c3183a3a14d5efc9cc2c3
Fernando Soares Da Silva <nandosoares719@gmail.com>
2025-09-30 09:24:39 -0300

    classe fechamento ordem servico

--- src/br/edu/cs/poo/ac/ordem/entidades/FechamentoOrdemServico.java
+++ src/br/edu/cs/poo/ac/ordem/entidades/FechamentoOrdemServico.java
@@ -1,5 +1,23 @@
 package br.edu.cs.poo.ac.ordem.entidades;
 
-public class FechamentoOrdemServico {
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class FechamentoOrdemServico {
+    private String numeroOrdemServico;
+    private LocalDate dataFechamento;
+    private boolean pago;
+    private String relatorioFinal;
+
+
+    public FechamentoOrdemServico(String numeroOrdemServico, LocalDate dataFechamento, boolean pago, String relatorioFinal) {
+        this.numeroOrdemServico = numeroOrdemServico;
+        this.dataFechamento = dataFechamento;
+        this.pago = pago;
+        this.relatorioFinal = relatorioFinal;
+    }
 }
